b2ac3034134976af9b39795eca4e5dbd24986abb
DESKTOP-IBJNLFM\MikeIt <mixaylo.skv@gmail.com>
2024-12-11 23:23:38 +0200

    add testing

--- BusinessLogic/ModelsDTO/BookDTO/CreateBookDTO.cs
+++ BusinessLogic/ModelsDTO/BookDTO/CreateBookDTO.cs
@@ -2,7 +2,7 @@
 {
     public class CreateBookDTO
     {
-        public string Title { get; set; }
+        public string? Title { get; set; }
         public string Description { get; set; }
         public int PageCount { get; set; }
         public decimal AnnouncedPrice { get; set; }
