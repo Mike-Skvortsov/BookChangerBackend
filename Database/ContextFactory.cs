8480aa2ef4f27975aeae030cbf5f190c614f43c1
DESKTOP-IBJNLFM\MikeIt <mixaylo.skv@gmail.com>
2024-12-13 12:30:42 +0200

    deploy

--- Database/ContextFactory.cs
+++ Database/ContextFactory.cs
@@ -11,19 +11,15 @@ namespace Database
     {
         public Context CreateDbContext(string[] args)
         {
-            // Визначення шляху до appsettings.json
             var basePath = Path.Combine(Directory.GetCurrentDirectory(), "../BooksChanger");
 
-            // Створення конфігурації для читання appsettings.json
             var configuration = new ConfigurationBuilder()
                 .SetBasePath(basePath) // Вказівка на директорію BooksChanger
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .Build();
 
-            // Отримання рядка підключення з appsettings.json
             var connectionString = configuration.GetConnectionString("DB");
 
-            // Налаштування DbContextOptionsBuilder
             var optionsBuilder = new DbContextOptionsBuilder<Context>();
             optionsBuilder.UseNpgsql(connectionString);
 
